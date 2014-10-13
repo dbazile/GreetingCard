c988122c1235b861b856859dd40a668cc5f4ffdb
David Bazile <davidbazile@gmail.com>
2014-10-13 16:38:03 -0400

    Implemented reading from JSON

--- GreetingCard/DataUtility.swift
+++ GreetingCard/DataUtility.swift
@@ -17,8 +17,34 @@ class DataUtility
 	///
 	class func AllCards() -> [Card]
 	{
-		return GenerateCards()
+//		Alamofire.request(.GET, "http://andsoitcontinues.com/sample_data/music.json")
+//			.responseJSON { (request, response, json, error) in
+//				let data = (json as NSArray)[0] as NSDictionary
+//				println(data["album_title"])
+//		}
+		
+		return ReadCardsFromLocalJson()
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	private class func ReadCardsFromLocalJson() -> [Card]
+	{
+		let path = NSBundle.mainBundle().pathForResource("cards", ofType: ".json") as String!
+		let jsonString = NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+
+		return unserialize(jsonString)
+	}
+	
+	
+	
+	
+	
 
 	///
 	/// Returns a list of identifiers for all local sprites
@@ -27,11 +53,6 @@ class DataUtility
 	{
 		var paths: [String] = []
 
-//		Alamofire.request(.GET, "http://andsoitcontinues.com")
-//			.response { (request, response, data, error) in
-//				println(response)
-//		}
-		
 		paths.append("backdrop-home-indoors")
 		paths.append("backdrop-road")
 		paths.append("backdrop-yellow")
@@ -159,4 +180,197 @@ class DataUtility
 	{
 		return NSBundle.mainBundle().pathForResource(basename, ofType:".png", inDirectory:"sprites")
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private class func serialize(cards: [Card]) -> String
+	{
+		func _layers(layers: [Layer]) -> [NSDictionary]
+		{
+			var container: [NSDictionary] = []
+			
+			for layer in layers {
+				var convertedLayer = NSMutableDictionary()
+				
+				convertedLayer["image"] = layer.image
+				convertedLayer["left"] = layer.left
+				convertedLayer["opacity"] = layer.opacity
+				convertedLayer["rotation"] = layer.rotation
+				convertedLayer["scale"] = layer.scale
+				convertedLayer["top"] = layer.top
+				convertedLayer["visible"] = layer.visible
+				
+				container.append(convertedLayer)
+			}
+			
+			return container
+		}
+		
+		func _scenes(scenes:[Scene]) -> [NSDictionary]
+		{
+			var container: [NSDictionary] = []
+			
+			for scene in scenes {
+				var convertedScene = NSMutableDictionary()
+				
+				convertedScene["caption"] = scene.caption
+				convertedScene["layers"] = _layers(scene.layers)
+				
+				container.append(convertedScene)
+			}
+			
+			return container
+		}
+		
+		var root = NSMutableDictionary()
+		var container = NSMutableArray()
+		
+		root["$version"] = "1.0.0"
+		root["$origin"] = "installer"
+		root["cards"] = container
+		
+		for card in cards {
+			var item = NSMutableDictionary()
+			
+			println("[data-util:serialize -> working on card '\(card.title)']")
+			
+			item["title"] = card.title
+			item["isNew"] = card.isNew
+			
+			item.setValue(_scenes(card.scenes), forKey:"scenes")
+			
+			container.addObject(item)
+		}
+		
+		let bytes = NSJSONSerialization.dataWithJSONObject(root, options:nil, error:nil)
+		let output = NSString(data:bytes!, encoding: NSUTF8StringEncoding)
+		
+		println("[data-util:serialize -> operation complete, JSON data follows]")
+		println(output)
+		println()
+		
+		return output
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private class func unserialize(json:String) -> [Card]
+	{
+		func _layers(raw: AnyObject?) -> [Layer]
+		{
+			var layers: [Layer] = []
+			
+			if let rawLayers = raw as NSArray? {
+				
+				for rawLayer in rawLayers {
+					let layer = Layer(
+						image: rawLayer["image"] as String,
+						visible: rawLayer["visible"] as Bool,
+						scale: rawLayer["scale"] as Float,
+						rotation: rawLayer["rotation"] as Int,
+						opacity: rawLayer["opacity"] as Float,
+						top: rawLayer["top"] as Int,
+						left: rawLayer["left"] as Int)
+					
+					layers.append(layer)
+				}
+				
+			}
+			
+			return layers
+		}
+		
+		
+		func _scenes(raw: AnyObject?) -> [Scene]
+		{
+			var scenes: [Scene] = []
+			
+			if let rawScenes = raw as NSArray? {
+				
+				for rawScene in rawScenes {
+					let scene = Scene(caption:rawScene["caption"] as String!, layers: _layers(rawScene["layers"]))
+					
+					scenes.append(scene)
+				}
+			}
+			
+			return scenes
+		}
+		
+		
+		var cards: [Card] = []
+		
+		let data = json.dataUsingEncoding(NSUTF8StringEncoding)!
+		let rawContainer = NSJSONSerialization.JSONObjectWithData(data, options:nil, error:nil) as NSDictionary
+		
+		let version = rawContainer["$version"] as String?
+		println("[data-util:unserialize -> now parsing cards.json (v\(version))]")
+		
+		let rawCards = rawContainer.valueForKey("cards") as [NSDictionary]
+		
+		for rawcard in rawCards {
+			let title = rawcard["title"] as String?
+			println("[data-util:unserialize -> working on raw card '\(title)']")
+			let card = Card(title:rawcard["title"] as String!, isNew:rawcard["isNew"] as Bool!, scenes: _scenes(rawcard["scenes"]))
+			
+			cards.append(card)
+		}
+		
+		println("[data-util:unserialize -> operation complete, data follows]")
+		println(cards)
+		
+		return cards
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	class func Export(card: Card) -> String
+	{
+		println("[data-util:Export -> exporting base64-encoded card]")
+		return encode(serialize([card]))
+	}
+	
+	class func Import(encoded: String) -> Card?
+	{
+		println("[data-util:Import -> attempting to import base64-encoded card]")
+		return unserialize(decode(encoded)).first
+	}
+	
+	private class func decode(encoded:String) -> String
+	{
+		let data = NSData(base64EncodedString:encoded, options:nil)
+		return NSString(data:data, encoding:NSUTF8StringEncoding)
+	}
+	
+	private class func encode(plaintext:String) -> String
+	{
+		let data = plaintext.dataUsingEncoding(NSUTF8StringEncoding)!
+		return data.base64EncodedStringWithOptions(nil)
+	}
+	
 }
