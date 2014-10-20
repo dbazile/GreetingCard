e7cfbbc4c39163013a569a3b7792daf413c070c2
David Bazile <davidbazile@gmail.com>
2014-10-20 00:20:20 -0400

    Removed Alamofire import

---
+++ GreetingCardTests/DataUtilityTest.swift
@@ -0,0 +1,36 @@
+//
+//  DataUtilityTest.swift
+//  GreetingCard
+//
+//  Created by David Bazile on 10/19/14.
+//  Copyright (c) 2014 David Bazile. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DataUtilityTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
